9eea9ee153f2ab2c9e351220edf6704b05bc54ab
Adam Brown <acourtneybrown@gmail.com>
2025-06-19 14:39:09 -0700

    Add `external_script` decorator, analogous to `script_filter` one

--- pyfred/tests/test_workflow.py
+++ pyfred/tests/test_workflow.py
@@ -2,7 +2,7 @@ import json
 from pathlib import PosixPath
 
 from pyfred.model import Environment, OutputItem, ScriptFilterOutput
-from pyfred.workflow import script_filter
+from pyfred.workflow import external_script, script_filter
 
 
 def test_decorator(capsys, monkeypatch):
@@ -56,3 +56,56 @@ def test_decorator(capsys, monkeypatch):
     output = json.loads(capsys.readouterr().out)
 
     assert output == {"items": [{"title": "Hello Alfred!", "type": "default"}]}
+
+
+def test_external_script_decorator(capsys, monkeypatch):
+    monkeypatch.setenv("alfred_preferences", "/Users/Crayons/Dropbox/Alfred/Alfred.alfredpreferences")
+    monkeypatch.setenv("alfred_version", "/Users/Crayons/Dropbox/Alfred/Alfred.alfredpreferences")
+    monkeypatch.setenv("alfred_preferences", "/Users/Crayons/Dropbox/Alfred/Alfred.alfredpreferences")
+    monkeypatch.setenv("alfred_preferences_localhash", "adbd4f66bc3ae8493832af61a41ee609b20d8705")
+    monkeypatch.setenv("alfred_theme", "alfred.theme.yosemite")
+    monkeypatch.setenv("alfred_theme_background", "rgba(255,255,255,0.98)")
+    monkeypatch.setenv("alfred_theme_subtext", "3")
+    monkeypatch.setenv("alfred_version", "5.0")
+    monkeypatch.setenv("alfred_version_build", "2058")
+    monkeypatch.setenv("alfred_workflow_bundleid", "com.alfredapp.googlesuggest")
+    monkeypatch.setenv(
+        "alfred_workflow_cache",
+        "/Users/Crayons/Library/Caches/com.runningwithcrayons.Alfred/Workflow Data/com.alfredapp.googlesuggest",
+    )
+    monkeypatch.setenv(
+        "alfred_workflow_data",
+        "/Users/Crayons/Library/Application Support/Alfred/Workflow Data/com.alfredapp.googlesuggest",
+    )
+    monkeypatch.setenv("alfred_workflow_name", "Google Suggest")
+    monkeypatch.setenv("alfred_workflow_version", "1.7")
+    monkeypatch.setenv("alfred_workflow_uid", "user.workflow.B0AC54EC-601C-479A-9428-01F9FD732959")
+    monkeypatch.setenv("alfred_debug", "1")
+
+    @external_script
+    def under_test(path, args, env):
+        assert path.exists()
+        assert isinstance(args, list)
+        assert env == Environment(
+            debug=True,
+            preferences_file=PosixPath("/Users/Crayons/Dropbox/Alfred/Alfred.alfredpreferences"),
+            version="5.0",
+            version_build="2058",
+            workflow_name="Google Suggest",
+            workflow_version="1.7",
+            workflow_bundle_id=None,
+            workflow_uid="user.workflow.B0AC54EC-601C-479A-9428-01F9FD732959",
+            workflow_cache=PosixPath(
+                "/Users/Crayons/Library/Caches/com.runningwithcrayons.Alfred/Workflow Data/com.alfredapp.googlesuggest"
+            ),
+            workflow_data=PosixPath(
+                "/Users/Crayons/Library/Application Support/Alfred/Workflow Data/com.alfredapp.googlesuggest"
+            ),
+        )
+        return ["abc", "def"]
+
+    under_test()
+
+    output = capsys.readouterr().out
+
+    assert output == "abc def\n"
--- pyfred/workflow.py
+++ pyfred/workflow.py
@@ -2,7 +2,7 @@ import json
 import logging
 import sys
 from pathlib import Path
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Optional, Union
 
 from pyfred.model import Environment, ScriptFilterOutput
 
@@ -43,5 +43,41 @@ def script_filter(fn: Callable[[Path, list[str], Optional[Environment]], ScriptF
             exit(1)
 
         print(json.dumps(output, default=vars_if_set))
+
+    return decorator
+
+
+def external_script(fn: Callable[[Path, list[str], Optional[Environment]], Union[str, list[str]]]):
+    """
+    Decorator for an external script
+
+    Preprocesses the input and parses environment variables. The main function of an external script should be decorated
+    with this.
+    """
+    path, args = (sys.argv[0], sys.argv[1:])
+    alfred_environment = Environment.from_env()
+
+    is_debug = alfred_environment is None or alfred_environment.debug
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.DEBUG if is_debug else logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    def decorator():
+        output = fn(Path(path), args, alfred_environment)
+
+        if not isinstance(output, str) and not isinstance(output, list):
+            logging.error(
+                "The workflow returned an unexpected type: %s, but expected %s.%s.",
+                type(output),
+                ScriptFilterOutput.__module__,
+                ScriptFilterOutput.__name__,
+            )
+            logging.debug("Unexpected instance of type %s: %s", type(output), repr(output))
+            exit(1)
+
+        print(*output if isinstance(output, list) else output)
 
     return decorator
